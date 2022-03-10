a6464dcb5e6a217856613ff08bcdf81aa83ef373
Warintorn(Arm) <warintorn_wib@true-e-logistics.com>
2022-03-10 14:27:44 +0700

    constant

--- constant/main.go
+++ constant/main.go
@@ -3,12 +3,37 @@ package main
 import "fmt"
 
 func main() {
+	const value int = 5
+	const title = "Go For Brginner"
+
 	const (
-		sunday = iota
-		monday
-		tuesday
+		january = iota + 1
+		february
+		march
+		april
+		may
+		june
+		july
+		august
+		september
+		october
+		november
+		december
 	)
-	fmt.Println(sunday)
-	fmt.Println(monday)
-	fmt.Println(tuesday)
+
+	fmt.Println("value", value)
+	fmt.Println("title", title)
+
+	fmt.Println("january ==>", january)
+	fmt.Println("february ==>", february)
+	fmt.Println("march ==>", march)
+	fmt.Println("april ==>", april)
+	fmt.Println("may ==>", may)
+	fmt.Println("june ==>", june)
+	fmt.Println("july ==>", july)
+	fmt.Println("august ==>", august)
+	fmt.Println("september ==>", september)
+	fmt.Println("october ==>", october)
+	fmt.Println("november ==>", november)
+	fmt.Println("december ==>", december)
 }
